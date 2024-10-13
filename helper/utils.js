56eee186fd2ce2998948ddad3e27ec7a3b8b5874
Sherzod-Tohirov <sherzodtohirov862@gmail.com>
2024-10-13 21:37:32 +0500

    Add CRUD

--- helper/utils.js
+++ helper/utils.js
@@ -1,11 +1,12 @@
 const path = require("path");
-
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+const dbPath = path.join(__dirname, "..", "db", "todos.json");
+const db = require(dbPath);
 function readDataFromDbAsJSON() {
   try {
-    const dbPath = path.join(__dirname, "..", "db", "todos.json");
-    const data = require(dbPath);
-    if (data) {
-      return data;
+    if (db) {
+      return db;
     }
     return [];
   } catch (err) {
@@ -13,6 +14,37 @@ function readDataFromDbAsJSON() {
   }
 }
 
+function writeDataToDb(data) {
+  try {
+    const newTodo = {
+      id: uuidv4(),
+      title: data.title,
+      desc: data.desc,
+      completed: false,
+    };
+    db.push(newTodo);
+    storeTodosToDb(db);
+  } catch (err) {
+    console.log("Error while writing data to db: ", err);
+  }
+}
+
+function storeTodosToDb(todos) {
+  try {
+    fs.writeFile(dbPath, JSON.stringify(todos , null, 2), "utf8", (err) => {
+      if (err) {
+        console.log("Error while writing todos.json file: ", err);
+      } else {
+        console.log("Successfully wrote to todos.json file!");
+      }
+    });
+  } catch (err) {
+    console.log("Error while storing todos: ", err);
+  }
+}
+
 module.exports = {
   readDataFromDbAsJSON,
+  writeDataToDb,
+  storeTodosToDb,
 };
--- routes/todo.js
+++ routes/todo.js
@@ -1,20 +1,108 @@
 const { Router } = require("express");
-const { readDataFromDbAsJSON } = require("../helper/utils");
+const {
+  readDataFromDbAsJSON,
+  writeDataToDb,
+  storeTodosToDb,
+} = require("../helper/utils");
 const router = new Router();
 
 const todos = readDataFromDbAsJSON();
 
 router.get("/", (_, res) => {
   res.render("home", {
-     title: "Home page",
-     todos
+    title: "Home page",
+    todos,
   });
 });
 
 router.get("/task/new", (_, res) => {
-     res.render("add-todo", {
-          title: "Add new todo"
-     });
-   });
+  res.render("add-todo", {
+    title: "Add new todo",
+  });
+});
 
+router.post("/task", (req, res) => {
+  const title = req.body.todo_title;
+  const desc = req.body.todo_desc;
+  try {
+    writeDataToDb({ title, desc });
+    res.redirect("/");
+  } catch (err) {
+    console.log("Error while writing to the db: ", err);
+    res.status(400).json({
+      error: true,
+      message: "Server xatosi !",
+    });
+  }
+  if (!title || !desc) {
+    res.status(400).json({
+      error: true,
+      message: "Berilgan ma'lumotlar to'g'riligini tekshiring !",
+    });
+  }
+});
+
+router.post("/task/:id/update", (req, res) => {
+  if (!req.params.id) {
+    res.status(400).send({
+      error: true,
+      message: "Iltimos tahrirlash uchun vazifa idisini yuboring !",
+    });
+  }
+
+  const todoId = req.params.id;
+  const foundTodo = todos.find((todo) => todo.id === todoId);
+  if (!foundTodo) {
+    res.status(400).send({
+      error: true,
+      status: 400,
+      message: "Bu idga tegishli vazifa topilmadi !",
+    });
+  }
+  //  Update status only
+  if (Object.keys(req.body).length === 1 && req.body.completed !== undefined) {
+    foundTodo.completed = req.body.completed;
+    console.log("found todo: ", foundTodo);
+    storeTodosToDb(todos);
+    res.status(200).send({
+      success: true,
+      status: 200,
+      data: foundTodo,
+      message: "Muvaffaqiyatli o'zgartirildi !",
+    });
+  }
+  console.log("params: ", req.params);
+  console.log("body: ", req.body);
+  res.end();
+
+  //  Update
+});
+
+router.delete("/task/:id/delete", (req, res) => {
+  const id = req.params.id;
+
+  if (!id) {
+    res.status(400).send({
+      error: true,
+      status: 400,
+      message: "Iltimos idni yuboring !",
+    });
+  }
+
+  const foundTodoIndex = todos.findIndex((todo) => todo.id === id);
+  if (foundTodoIndex === -1) {
+    res.status(404).send({
+      message: "Not found !",
+      status: 404,
+    });
+  }
+
+  todos.splice(foundTodoIndex, 1);
+  storeTodosToDb(todos);
+  res.status(200).send({
+    success: true,
+    status: 200,
+    message: "Muvaffaqiyatli o'chirildi !",
+  });
+});
 module.exports = router;
